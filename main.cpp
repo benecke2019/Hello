b0aec5ea4ab475867ddf42379ba48b52df796db7
benecke2019 <fred1230@naver.com>
2020-04-24 14:08:43 +0900

    branch1

--- main.cpp
+++ main.cpp
@@ -3,15 +3,33 @@
 int main(void)
 {
 	int a = 0;
+	float b = 0;
+	char type;
 
-	scanf("%d", &a);
-
-	if (a > 0)
-		printf("'a'가 양수입니다.\n");
-	else if (a == 0)
-		printf("'a'가 0입니다.\n");
+	printf("data type을 입력하세요 (I/F):");
+	scanf("%c", &type);
+	
+	if (type == 'I')
+	{
+		scanf("%d", &a);
+		if (a > 0)
+			printf("'a'가 양수입니다.\n");
+		else if (a == 0)
+			printf("'a'가 0입니다.\n");
+		else
+			printf("'a'가 음수입니다.\n");
+	}
 	else
-		printf("'a'가 음수입니다.\n");
+	{
+		scanf("%f", &b);
+		if (b > 0)
+			printf("'b'가 양수입니다.\n");
+		else if (b == 0)
+			printf("'b'가 0입니다.\n");
+		else
+			printf("'b'가 음수입니다.\n");
+	}
+
 
 	return 0;
 }
