ccb74c33265019a3e3f4a885fbbb0b944ed23a4d
benecke2019 <fred1230@naver.com>
2020-04-24 14:27:36 +0900

    branch2

--- main.cpp
+++ main.cpp
@@ -6,30 +6,35 @@ int main(void)
 	float b = 0;
 	char type;
 
-	printf("data type을 입력하세요 (I/F):");
-	scanf("%c", &type);
-	
-	if (type == 'I')
+	for (;;)
 	{
-		scanf("%d", &a);
-		if (a > 0)
-			printf("'a'가 양수입니다.\n");
-		else if (a == 0)
-			printf("'a'가 0입니다.\n");
-		else
-			printf("'a'가 음수입니다.\n");
-	}
-	else
-	{
-		scanf("%f", &b);
-		if (b > 0)
-			printf("'b'가 양수입니다.\n");
-		else if (b == 0)
-			printf("'b'가 0입니다.\n");
-		else
-			printf("'b'가 음수입니다.\n");
-	}
+		printf("data type을 입력하세요 (I/F):");
+		scanf("%c", &type);
 
-
+		if (type == 'Q')
+			break;
+		else if (type == 'I')
+		{
+			scanf("%d", &a);
+			if (a > 0)
+				printf("'a'가 양수입니다.\n");
+			else if (a == 0)
+				printf("'a'가 0입니다.\n");
+			else
+				printf("'a'가 음수입니다.\n");
+		}
+		else if (type == 'F')
+		{
+			scanf("%f", &b);
+			if (b > 0)
+				printf("'b'가 양수입니다.\n");
+			else if (b == 0)
+				printf("'b'가 0입니다.\n");
+			else
+				printf("'b'가 음수입니다.\n");
+		}
+		else
+			printf("Wrong type!\n");
+	}
 	return 0;
 }
